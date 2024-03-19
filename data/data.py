7eaa630b153c9b00553c8f1d8b01643950466122
YURI <statio2009@gmail.com>
2024-03-19 21:29:02 +0400

    introduce type annotations into the project

--- data/data.py
+++ data/data.py
@@ -5,7 +5,15 @@ from faker import Faker
 fake = Faker()
 
 
-def convert_post_code_to_name(post_code_digits: str) -> str:
+def _convert_post_code_to_name(post_code_digits: str) -> str:
+    """Преобразует последовательность цифр почтового индекса в соответствующее имя.
+
+    Args:
+        post_code_digits (str): Последовательность цифр почтового индекса.
+
+    Returns:
+        str: Соответствующее имя.
+    """
     post_code_numbers_list = [int(post_code_digits[x:x + 2]) for x in range(0, 10, 2)]
 
     # в соответствии с ascii таблицей символ "а" код "97"
@@ -19,6 +27,13 @@ def convert_post_code_to_name(post_code_digits: str) -> str:
 
 @dataclass
 class Customer:
+    """ Представляет собой клиента.
+
+    Attributes:
+        post_code (str): Почтовый индекс клиента.
+        first_name (str): Преобразованное имя клиента.
+        last_name (str): Фамилия клиента.
+    """
     post_code: str = fake.numerify('##########')
-    first_name: str = convert_post_code_to_name(post_code)
+    first_name: str = _convert_post_code_to_name(post_code)
     last_name: str = fake.last_name()
--- pages/list_customers_page.py
+++ pages/list_customers_page.py
@@ -6,6 +6,16 @@ from pages.base_page import BasePage
 
 
 class ListCustomersPage(BasePage):
+    """Страница со списком клиентов.
+
+    Attributes:
+        TABLE_DATA_ROWS_LIST (tuple): Локатор списка строк таблицы данных.
+        FIRST_NAME_TITLE (tuple): Локатор заголовка "First Name".
+        FIRST_NAMES_LIST (tuple): Локатор списка элементов "First Name".
+        LAST_NAMES_LIST (tuple): Локатор списка элементов "Last Name".
+        POST_CODE_LIST (tuple): Локатор списка элементов "Post Code".
+        DELETE_BTN_LIST (tuple): Локатор списка кнопок удаления записей.
+    """
     TABLE_DATA_ROWS_LIST = ("xpath", "(//table//tbody//tr)")
     FIRST_NAME_TITLE = ("xpath", "//a[contains(text(),'First Name')]")
 
@@ -15,16 +25,23 @@ class ListCustomersPage(BasePage):
     DELETE_BTN_LIST = ("xpath", "(//button[@ng-click='deleteCust(cust)'])")
 
     def sort_by_first_name(self) -> None:
+        """Сортирует записи по имени."""
         column = self.element_to_be_clickable(self.FIRST_NAME_TITLE)
         column.click()
         column.click()
 
     def delete_customers(self) -> None:
+        """Удаляет выбранных клиентов."""
         delete_records_element_list = self.get_delete_records_element_list()
         for elem in delete_records_element_list:
             elem[3].click()
 
     def get_all_records_elements_list(self) -> List[Tuple[WebElement, ...]]:
+        """Получает список всех элементов записей.
+
+        Returns:
+            List[Tuple[WebElement, ...]]: Список кортежей элементов записей.
+        """
         count_data_row = len(self.presence_of_all_elements_located(self.TABLE_DATA_ROWS_LIST))
 
         all_records_elements_list = []
@@ -45,12 +62,17 @@ class ListCustomersPage(BasePage):
         return all_records_elements_list
 
     def get_delete_records_element_list(self) -> List[Tuple[WebElement, ...]]:
+        """Получает список элементов записей, подлежащих удалению.
+
+        Returns:
+            List[Tuple[WebElement, ...]]: Список кортежей элементов записей для удаления.
+        """
         all_records_element_list = self.get_all_records_elements_list()
         if not all_records_element_list:
             return []
 
         lengths_names_list = [len(elem[0].text) for elem in all_records_element_list]
-        comparison_value = self.value_with_min_deviation_from_avg(lengths_names_list)
+        comparison_value = self._value_with_min_deviation_from_avg(lengths_names_list)
 
         delete_records_element_list = [
             elem for elem in all_records_element_list if len(elem[0].text) == comparison_value
@@ -59,6 +81,7 @@ class ListCustomersPage(BasePage):
         return delete_records_element_list
 
     def get_all_data_customers(self) -> List[Tuple[str, ...]]:
+        """"""
         all_records_element_list = self.get_all_records_elements_list()
         if not all_records_element_list:
             return []
@@ -68,7 +91,18 @@ class ListCustomersPage(BasePage):
         return all_data_customers
 
     @staticmethod
-    def value_with_min_deviation_from_avg(number_list: List[int]) -> int:  # type: ignore
+    def _value_with_min_deviation_from_avg(number_list: List[int]) -> int:  # type: ignore
+        """Находит значение с минимальным отклонением от среднего.
+
+        Args:
+            number_list (List[int]): Список чисел.
+
+        Returns:
+            int: Значение с минимальным отклонением от среднего.
+
+        Raises:
+            ValueError: Если список чисел пуст.
+        """
         if not number_list:
             raise ValueError("List of numbers must not be empty")
 
